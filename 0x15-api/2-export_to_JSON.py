572efbfb4b5ce774aa520d1a9663e8a06a29411b
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-15 04:21:04 +0000

    [fix]: int v2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    employee_id = argv[1]
+    employee_id = int(argv[1])
     URL_EMPLOYEE = "https://jsonplaceholder.typicode.com/users/{}".format(
         employee_id)
     URL_TASKS = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
@@ -18,9 +18,9 @@ if __name__ == "__main__":
     tasks = requests.get(URL_TASKS).json()
 
     data = {}
-    data[employee_id] = []
+    data[argv[1]] = []
     for task in tasks:
-        data[employee_id.append({
+        data[argv[1]].append({
             'task': task.get('title'),
             'completed': task.get('completed'),
             'username': employee.get('name')
