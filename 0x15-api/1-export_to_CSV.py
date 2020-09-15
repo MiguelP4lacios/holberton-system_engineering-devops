4e0510308a434de6fdb24537281f7ea276f1d1d0
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-15 04:03:12 +0000

    fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,16 +9,16 @@ from sys import argv
 if __name__ == "__main__":
 
     employee_id = int(argv[1])
-    URL_EMPLOYEE = "https://jsonplaceholder.typicode.com/users/{}".format(
-        employee_id)
-    URL_TASKS = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        employee_id)
 
-    employee = requests.get(URL_EMPLOYEE).json()
-    tasks = requests.get(URL_TASKS).json()
+    employee = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(
+            employee_id)).json()
+    tasks = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            employee_id)).json()
 
     with open('{}.csv'.format(employee_id), mode='w') as file:
         _writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in tasks:
             _writer.writerow([employee_id, employee.get('name'),
-                             task.get('completed'), task.get('title')])
+                              task.get('completed'), task.get('title')])
