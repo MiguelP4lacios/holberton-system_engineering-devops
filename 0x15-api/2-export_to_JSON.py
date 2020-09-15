f0059c598773229aeff2c7f27df5fe8d2bf922ad
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-15 03:26:57 +0000

    [feature]: to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Using what you did in the task #0, extend your
+Python script to export data in the JSON format.
+"""
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    employee_id = int(argv[1])
+    URL_EMPLOYEE = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
+    URL_TASKS = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        employee_id)
+
+    employee = requests.get(URL_EMPLOYEE).json()
+    tasks = requests.get(URL_TASKS).json()
+
+    EMPLOYEE_NAME = employee.get('name')
+
+    data = {}
+    data[str(employee_id)] = []
+    for task in tasks:
+        data[str(employee_id)].append({
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': EMPLOYEE_NAME
+        })
+    with open("{}.json".format(employee_id), mode='w') as file:
+        json.dump(data, file)
