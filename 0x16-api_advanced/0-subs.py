a7ca13bf1b4813fcd06837aea10a240bbf71ec24
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-17 19:09:49 +0000

    [feat]: subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module API
+"""
+
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit API and
+    returns the number of subscribers 
+    """
+
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {'User-agent': 'RED'}
+    res = requests.get(url=URL, headers=headers)
+    try:
+        return int(res.json().get('data').get('subscribers'))
+    except:
+        return 0
