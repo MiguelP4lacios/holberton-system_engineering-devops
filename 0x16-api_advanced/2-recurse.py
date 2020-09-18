253a643810ba3f127eea5b77fc77894be27e26ee
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-18 02:54:36 +0000

    [fix]: pep8+

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,8 +6,7 @@ import requests
 import sys
 
 
-    
-def recurse(subreddit, hot_list=[], after = None):
+def recurse(subreddit, hot_list=[], after=None):
     """function that queries the Reddit API and returns a list
     containing the titles of all hot articles for a given
     subreddit."""
