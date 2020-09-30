6a6c71541a59aeb43e785fe56cfdaecbe4cc632e
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-30 16:04:38 +0000

    [Feat]: mysql

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Puppet that replaces phpp → php in wp-settings.php
 
 exec { 'replace':
-    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+    command => 'sudo apt-get mysql && service mysql start'
 }
